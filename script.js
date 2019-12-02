5a734bb6bdff78d1d7f46a44a64f6bc2518a86a9
aom19 <39848284+aom19@users.noreply.github.com>
2019-12-02 23:27:18 +0200

    Edit code , function

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
 var num_of_squares = 6;
-var colors = generate_random_color(num_of_squares);
-var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
-var colorDisplay = document.getElementById("colorDisplay");
-var message_display = document.getElementById("message");
-var h1 = document.querySelector("h1");
-var reset_button = document.getElementById("reset");
-var mode_buttons = document.querySelectorAll(".mode");
+var colors = [];
+var pickedColor ;
 
+var squares = 			document.querySelectorAll(".square");
+var colorDisplay = 		document.getElementById("colorDisplay");
+var message_display = 	document.getElementById("message");
+var h1 = 				document.querySelector("h1");
+var reset_button = 		document.getElementById("reset");
+var mode_buttons = 		document.querySelectorAll(".mode");
 
-for ( var i = 0 ; i < mode_buttons.length; i++){
-	mode_buttons[i].addEventListener("click" , function(){
-		mode_buttons[0].classList.remove("selected");
-		mode_buttons[1].classList.remove("selected");
-		this.classList.add("selected");
-		if ( this.textContent === "Easy"){
-			num_of_squares = 3;
-		}else{
-			num_of_squares = 6;
-		}
-		reset();
+init();
 
-	});
+//FUNCTION INIT
+function init(){
+	set_mode();
+	set_squares();
+ 	reset();
+}
+//FUNCTION SET MODEL EASY OR HARD
+function set_mode(){
+	for ( var i = 0 ; i < mode_buttons.length; i++){
+		mode_buttons[i].addEventListener("click" , function(){
+			mode_buttons[0].classList.remove("selected");
+			mode_buttons[1].classList.remove("selected");
+			this.classList.add("selected");
+			this.textContent === "Easy" ? num_of_squares = 3: num_of_squares = 6;
+			reset();
+		});
+	}
 
 }
+
+function set_squares(){
+	for(var i = 0 ; i < squares.length ; i++){
+ 		//add click listener;
+		squares[i].addEventListener("click", function(){
+			var clicked_color = this.style.backgroundColor;
+ 			//compare the color;
+	
+			if(clicked_color === pickedColor){
+				message_display.textContent = "Correct!"
+				reset_button.textContent = "Play again ?"
+				change_colors(clicked_color); 
+				h1.style.backgroundColor = clicked_color;
+			}else {
+ 				this.style.backgroundColor="#232323";
+ 				message_display.textContent= "Try Again";
+			}
+ 		});
+ 	}
+}
+
 
 function reset(){
 	colors = generate_random_color(num_of_squares);
@@ -43,61 +70,32 @@ function reset(){
  	h1.style.backgroundColor = "steelblue";
  };
 
-
-
-
-
-
  reset_button.addEventListener("click", function(){
  	reset();
  });
 
- colorDisplay.textContent = pickedColor;
-
-
-
-
-for(var i = 0 ; i < squares.length ; i++){
-	squares[i].style.backgroundColor = colors[i];
- 	
-squares[i].addEventListener("click", function(){
-	var clicked_color = this.style.backgroundColor;
- 		
-	if(clicked_color === pickedColor){
-			message_display.textContent = "Correct!"
-			reset_button.textContent = "Play again ?"
-		change_colors(clicked_color); 
-			h1.style.backgroundColor = clicked_color;
-		}else {
- 			this.style.backgroundColor="#232323";
- 			message_display.textContent= "Try Again";
-		}
- 	});
- }
-
+ 
  function change_colors(color){
  		for( var i = 0 ; i < squares.length ; i++){
  			squares[i].style.backgroundColor = color;
  	}
-
-
  }
+
+
  function pickColor(){
  	var random = Math.floor(Math.random() * colors.length);
  	return colors[random];
  }
-
+//GENERATE RANDOM COLORS TO SQUARES
  function generate_random_color(num){
  	var arr = [];
  	for (var i = 0 ; i < num ; i++){
  		arr.push(random_color());
-
  	}
-
  	return arr;
  }
+// RANDOM RGB COLOR  - RED GREEN BLUE
  function random_color(){
-
  		var r = Math.floor(Math.random() * 256);
  		var g = Math.floor(Math.random() * 256);
  		var b = Math.floor(Math.random() * 256);
